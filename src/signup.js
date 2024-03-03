e6bccc0d3561aef9cdebd1f9f4d10e06c70c963e
PujaPriyadarsini <150437223+PujaPriyadarsini@users.noreply.github.com>
2024-03-04 03:53:09 +0530

    Update signup.js

--- src/signup.js
+++ src/signup.js
@@ -21,25 +21,38 @@ function Signup(props)
     }
 
 return(
-<div className=" bg-black p-10">
-<div className="bg-[#EFEFEF] p-10 border rounded-md">
-<h1 className="text-3xl font-medium">Hey Hi</h1>
-<p>I help you manage your activities after you login </p>
-<div className="flex flex-col gap-2 my-2">
-<input type="text" onClick={handleUInput}
-className="w-52 ☐ border-black p-1 bg-transparent border rounded-md" placeholder="username"/>
+    <div className="bg-black p-10">
+    <div className="bg-[#EFEFEF] p-10 border rounded-md">
+        <h1 className="text-3xl font-medium">Hey Hi</h1>
+        <p>Sign up here :)</p>
 
-<input type="text" onClick={handlePInput}
-className="w-52 ☐ border-black p-1 bg-transparent border rounded-md" placeholder="password"/>
+        <div className="flex flex-col gap-2 my-2">
+            <input 
+            type="text" 
+            className="w-52 border-black p-1 bg-transparent border rounded-md" 
+            placeholder="username"
+            onChange={handleUInput}
+            />
 
-<input type="text"
-className="w-52 ☐ border-black p-1 bg-transparent border rounded-md" placeholder="conform password"/>
+<input 
+            type="text" 
+            className="w-52 border-black p-1 bg-transparent border rounded-md" 
+            placeholder="password"
+            onChange={handlePInput}
+            />
 
-<button onClick={addUser} className="bg-[#FCA201] w-24 rounded-md p-1">Sign Up</button>
-<p>Already have an accout? <Link to="/" className="text-red-500 underline">login</Link></p>
+<input 
+            type="text" 
+            className="w-52 border-black p-1 bg-transparent border rounded-md" 
+            placeholder="confirm password"/>
 
-</div>
-</div>
+            <button className="bg-[#FCA201] w-24 p-1 rounded-md" onClick={addUser}>
+                Sign Up
+            </button>
+
+            <p>Already have an account? <Link to={"/"} className="underline">Login</Link>  </p>
+        </div>
+    </div>
 </div>
 )
 }
